d22d7482b92232db3583d488bb7f5688bd512143
Ayana-wq <ayan.mambetova@gmail.com>
2024-10-16 21:32:08 -0500

    first commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/kishan.jpeg";
 import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function About() {
   return (
     <div className="profile">
-      <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
-      <h2 className="profile-text">The creator of this awesome pokedex</h2>
-      <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
-      </h4>
+      
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
+        <a href="">
           <FaYoutube />
         </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
+        <a href="">
           <FaLinkedin />
         </a>
       </div>
